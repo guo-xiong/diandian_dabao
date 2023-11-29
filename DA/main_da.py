ae0c1653485e4b887bac23882d7c96d925a9bc04
guo <1390468953@qq.com>
2023-11-29 20:49:40 +0800

    添加fecaBook渠道删除第一个spript功能

--- DA/main_da.py
+++ DA/main_da.py
@@ -21,6 +21,19 @@ def replaceFile(readPath,writePath,new):
         with open(writePath, 'w',encoding='utf-8') as file:
             file.write(filedata)
 
+#从str1一直删除直到str2
+def remove_between_strings(input_str, start_str, end_str):
+    start_index = input_str.find(start_str)
+    end_index = input_str.find(end_str, start_index + len(start_str))
+
+    if start_index != -1 and end_index != -1:
+        result_str = input_str[:start_index] + input_str[end_index + len(end_str):]
+        return result_str
+    else:
+        # 如果未找到指定的子字符串，则返回原始字符串
+        return input_str
+
+
 path = input("文件路径：")
 #项目名
 da_name = input("项目名:")
@@ -41,6 +54,13 @@ print("Facebook")
 newFile = path + "\\"+da_name+"_Facebook\\index.html"
 copy(indexFile,newFile)
 copy(res,path + "\\"+da_name+"_Facebook\\res.js")
+#删除第一个script
+with open(newFile, 'r',encoding='utf-8') as file:
+    filedata = file.read()
+    filedata = remove_between_strings(filedata,"<script","</script>")
+    with open(newFile, 'w',encoding='utf-8') as file:
+            file.write(filedata)
+            
 with open("DA\\Facebook_tz.txt",'r',encoding='utf-8') as file:
     new = file.read()
     replaceFile("DA\\log.txt",newFile,new)
@@ -48,6 +68,9 @@ with open("DA\\Facebook_tz.txt",'r',encoding='utf-8') as file:
 with open("DA\\Facebook_head_new.txt",'r',encoding='utf-8') as file:
     new = file.read()
     replaceFile("DA\\Facebook_head.txt",newFile,new)
+
+
+
 
 print("Google")
 #Google
